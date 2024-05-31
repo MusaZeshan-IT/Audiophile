5e3871d08acae46101ee66aff3a8df4a6b4161a6
Musa Zeshan <musazeshan.it@gmail.com>
2024-05-31 17:12:10 +0500

    The modal is now capable of displaying the product details of the products added to the cart.

--- src/components/Cart/Modal.jsx
+++ src/components/Cart/Modal.jsx
@@ -1,19 +1,21 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef } from 'react';
 import { CartContext } from '../../context/CartContext';
-import ShoppingCart from '../../assets/cart.svg'
+import ShoppingCart from '../../assets/cart.svg';
 import ProductList from '../../helpers/ProductList';
 
 function Modal({ handleCloseModal, showModal }) {
-    const { totalItemsInCart, removeAllItemsFromCart, cartItems } = useContext(CartContext)
-    const modalRef = useRef(null)
+    const { totalItemsInCart, removeAllItemsFromCart, cartItems } = useContext(CartContext);
+    const modalRef = useRef(null);
 
-    const productAddedToCartList = ProductList.filter((product) => {
-        if (cartItems[product.id] > 0) {
-            return product
+    const productAddedToCartList = ProductList.filter((product) => cartItems[product.id] > 0);
+
+    function handlePrice(product) {
+        if (product.price > 999) {
+            return product.price.toLocaleString();
+        } else {
+            return product.price;
         }
-    })
-
-    console.log(productAddedToCartList);
+    }
 
     useEffect(() => {
         function handleClickOutside(e) {
@@ -22,43 +24,58 @@ function Modal({ handleCloseModal, showModal }) {
             }
         }
         if (showModal) {
-            addEventListener('mousedown', handleClickOutside)
+            document.addEventListener('mousedown', handleClickOutside);
         } else {
-            removeEventListener('mousedown', handleClickOutside)
+            document.removeEventListener('mousedown', handleClickOutside);
         }
-        return () => removeEventListener('mousedown', handleClickOutside)
-    }, [showModal, handleCloseModal])
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, [showModal, handleCloseModal]);
 
     if (showModal === false) {
         return null;
     }
-    else {
-        return (
-            <div className='fixed overflow-hidden inset-0 bg-black bg-opacity-[0.4] flex justify-end items-start'>
-                {/* Modal starts from here */}
-                <div ref={modalRef} className='me-48 overflow-hidden mt-32 bg-white min-h-48 w-[370px] rounded-md p-7'>
-                    {totalItemsInCart > 0 ? (
-                        <div className='w-full flex justify-between'>
-                            <span className='text-lg font-black tracking-[2px]'>
-                                <span>CART </span>
-                                <span className='text-lg'>(</span>
-                                <span className='text-[18.5px]'>{totalItemsInCart}</span>
-                                <span className='text-lg'>)</span>
-                            </span>
-                            <button onClick={removeAllItemsFromCart} className='text-gray-400 tracking-wide underline'>Remove All</button>
-                        </div>
-                    ) : (
-                        <div className='w-full flex flex-col items-center'>
-                            <p className='text-[rgb(141,141,141)] font-semibold tracking-[0.6px]'>Your cart is empty</p>
-                            <img className='mt-4' src={ShoppingCart} alt="" height="100px" width="100px" />
-                        </div>
-                    )}
 
+    return (
+        <div className='fixed overflow-hidden inset-0 bg-black bg-opacity-[0.4] flex justify-end items-start'>
+            {/* Modal starts from here */}
+            <div ref={modalRef} className='me-48 overflow-hidden mt-32 bg-white min-h-48 w-[370px] rounded-md p-7'>
+                {totalItemsInCart > 0 ? (
+                    <div className='w-full flex justify-between'>
+                        <span className='text-lg font-black tracking-[2px]'>
+                            <span>CART </span>
+                            <span className='text-lg'>(</span>
+                            <span className='text-[18.5px]'>{totalItemsInCart}</span>
+                            <span className='text-lg'>)</span>
+                        </span>
+                        <button onClick={removeAllItemsFromCart} className='text-gray-400 tracking-wide underline'>
+                            Remove All
+                        </button>
+                    </div>
+                ) : (
+                    <div className='w-full flex flex-col items-center'>
+                        <p className='text-[rgb(141,141,141)] font-semibold tracking-[0.6px]'>Your cart is empty</p>
+                        <img className='mt-4' src={ShoppingCart} alt="" height="100px" width="100px" />
+                    </div>
+                )}
+
+                <div className='w-full flex flex-col mt-6'>
+                    {productAddedToCartList.map((product) => (
+                        <div key={product.id} className='flex mb-6'>
+                            <img className='h-16 w-16 rounded-lg' src={product.image} alt={product.name} />
+                            <div className='flex flex-col ml-4 justify-center ms-[9px]'>
+                                <p className='text-[rgb(44,44,44)] font-black tracking-[1px] text-[15.3px]'>{product.shortName}</p>
+                                <p className='mt-1 text-[rgb(160,160,160)] font-semibold tracking-widest text-[13px]'>
+                                    <span className='me-[1.6px]'>$</span>
+                                    {handlePrice(product)}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                {/* Modal ends here */}
             </div>
-        )
-    }
+            {/* Modal ends here */}
+        </div>
+    );
 }
 
-export default Modal
+export default Modal;
