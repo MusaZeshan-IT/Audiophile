390da1df7f877e5aacb41ae1dbb58f5e590a0a68
Musa Zeshan <musazeshan.it@gmail.com>
2024-06-11 13:24:08 +0500

    Now the navbar has been made fully responsive with one issue that it you can move the body when the nav menu is open but the main problme is that when you scroll down it shows the content that has been left upwards does not have the overlay on it.

--- src/components/Common/NavMenuModal.jsx
+++ src/components/Common/NavMenuModal.jsx
@@ -22,8 +22,8 @@ function NavMenuModal({ showMenu, handleCloseMenu }) {
     }
     return (
         <div className='z-30 lg:hidden block fixed inset-0 mt-[88px] bg-black bg-opacity-[0.9] h-full w-full'>
-            <div className='flex justify-center items-center overflow-auto bg-white h-72 w-full rounded-b-[10px] p-4 pointer-events-auto'>
-                <div className='h-full flex justify-between items-center w-full pt-[85px]'>
+            <div className='flex xs-custom:flex-row flex-col xs-custom:justify-center items-center bg-white overflow-hidden max-h-[80vh] w-full rounded-b-[10px] p-4'>
+                <div className='flex xs-custom:flex-row flex-col md-custom:gap-x-9 md:gap-x-6 xs-custom:gap-x-4 gap-y-24 justify-between items-center overflow-auto w-full pt-[80px] pb-5 sm:px-4 px-2'>
                     <NavListItem isNavMenu={true} handleCloseMenu={handleCloseMenu} name="HEADPHONES" image={Headphones} path="/headphones" />
                     <NavListItem isNavMenu={true} handleCloseMenu={handleCloseMenu} name="SPEAKERS" image={Speakers} path="/speakers" />
                     <NavListItem isNavMenu={true} handleCloseMenu={handleCloseMenu} name="EARPHONES" image={Earphones} path="/earphones" />
--- src/components/Shared/NavListItem.jsx
+++ src/components/Shared/NavListItem.jsx
@@ -20,12 +20,12 @@ function NavListItem({ name, image, path, handleCloseMenu, isNavMenu = false }) 
                     </Link>
                 </div>
             ) : (
-                <div onClick={() => handleCloseMenu()} className='bg-[rgb(238,238,238)] h-[150px] w-full rounded-lg shadow-md shadow-gray-300'>
+                <div onClick={() => handleCloseMenu()} className='w-full bg-[rgb(238,238,238)] xl-custom:h-[190px] h-[180px] rounded-lg shadow-md shadow-gray-300'>
                     <Link to={path}>
                         <div className='flex flex-col items-center'>
-                            <img src={image} alt="" className='h-32 object-cover relative bottom-12' />
-                            <div className='relative bottom-[52px] flex flex-col items-center'>
-                                <h1 className='mb-[8px] font-semibold tracking-[2px]'>{name}</h1>
+                            <img src={image} alt="" className='md:h-[160px] h-[155px] relative bottom-14' />
+                            <div className='relative md:bottom-[67px] bottom-[60px] flex flex-col items-center'>
+                                <h1 className='text-lg mb-2 font-semibold tracking-[2px]'>{name}</h1>
                                 <div>
                                     <span className='me-2 text-gray-500 text-[12px] font-semibold tracking-[1.5px]'>SHOP</span>
                                     <i className='fa-solid fa-chevron-right text-orange-600 text-[13px]'></i>
