2d51dd682b4963dcae176f39f950cfe37596b02b
Musa Zeshan <musazeshan.it@gmail.com>
2024-06-12 15:33:03 +0500

    Now my website is fully responsive and shows notifications. The last problem of the home card big 2 component has been fixed and made fully responsive.

--- src/components/Home/HomeCardBig2.jsx
+++ src/components/Home/HomeCardBig2.jsx
@@ -4,30 +4,52 @@ import CTA from '../Shared/ToViewCTA';
 import './HomeCardBig2.css'
 import { Link } from 'react-router-dom';
 
-function HomeCardBig2({ pathName, height, titleSize, titleColor, productImage, titleLetterSpacing = '2.2px', titleName }) {
+function HomeCardBig2({ pathName, height, bgImage, bgImageTablet, titleSize, titleLetterSpacing = '2.2px', titleName }) {
     const mainContDefaultCls = 'flex overflow-hidden justify-center mb-10 rounded-lg';
 
     return (
-        <div className={`lg:bg-[20%] md-custom:bg-[35%] md:bg-[45%] sm-custom:bg-[55%] sm:bg-[83%] xs-custom:bg-[81.5%] xs:bg-[78%] bg-[76%] home-card2-bg-cont lg:h-[330px] sm-custom:h-[450px] h-[500px] ${mainContDefaultCls}`} style={{ padding: '0 24px', justifyContent: 'start', height: height }}>
-            <div className='w-full h-full flex flex-col sm-custom:items-start sm-custom:ps-24 items-center justify-center z-[1]'>
-                <h2 className='text-center text-6xl sm-custom:text-black text-[rgb(255,255,255)] drop-shadow-2xl font-black' style={{ marginBottom: '35px', fontSize: titleSize, letterSpacing: titleLetterSpacing }}>
-                    {titleName}
-                </h2>
-                <Link to={pathName}>
-                    <button
-                        onMouseEnter={e => {
-                            e.currentTarget.style.backgroundColor = 'white'
-                            e.currentTarget.style.color = 'black'
-                        }}
-                        onMouseLeave={e => {
-                            e.currentTarget.style.backgroundColor = 'black'
-                            e.currentTarget.style.color = 'white'
-                        }}
-                        style={{transition: 'all 0.4s ease' }}
-                        className={`border sm-custom:border-black border-white font-semibold tracking-[2px] w-44 sm-custom:text-black text-white text-[12px] px-[28px] py-[13.5px] rounded-[2.5px]`}>SEE PRODUCT</button>
-                </Link>
+        <>
+            <div className={`lg:block hidden lg:h-[330px] sm-custom:h-[450px] h-[500px] ${mainContDefaultCls}`} style={{ padding: '0 24px', backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', justifyContent: 'start', height: height }}>
+                <div className='w-full h-full flex flex-col sm-custom:items-start sm-custom:ps-24 items-center justify-center z-[1]'>
+                    <h2 className='text-center text-6xl text-black drop-shadow-2xl font-black' style={{ marginBottom: '35px', fontSize: titleSize, letterSpacing: titleLetterSpacing }}>
+                        {titleName}
+                    </h2>
+                    <Link to={pathName}>
+                        <button
+                            onMouseEnter={e => {
+                                e.currentTarget.style.backgroundColor = 'black'
+                                e.currentTarget.style.color = 'white'
+                            }}
+                            onMouseLeave={e => {
+                                e.currentTarget.style.backgroundColor = ''
+                                e.currentTarget.style.color = 'black'
+                            }}
+                            style={{ transition: 'all 0.4s ease' }}
+                            className={`border sm-custom:border-black border-white font-semibold tracking-[2px] w-44 sm-custom:text-black text-white text-[12px] px-[28px] py-[13.5px] rounded-[2.5px]`}>SEE PRODUCT</button>
+                    </Link>
+                </div>
             </div>
-        </div>
+            <div className={`md:bg-center bg-[50%] lg:hidden lg:h-[330px] sm-custom:h-[450px] sm:h-[400px] h-[300px] ${mainContDefaultCls}`} style={{ padding: '0 24px', backgroundImage: `url(${bgImageTablet})`, backgroundSize: 'cover', justifyContent: 'start', height: height }}>
+                <div className='w-full h-full flex flex-col md:ps-24 sm-custom:ps-20 sm:ps-16 xs-custom:ps-14 xs:ps-10 2xs-custom:ps-8 2xs:ps-6 ps-2 justify-center z-[1]'>
+                    <h2 className='text-6xl text-black drop-shadow-2xl font-black' style={{ marginBottom: '35px', fontSize: titleSize, letterSpacing: titleLetterSpacing }}>
+                        {titleName}
+                    </h2>
+                    <Link to={pathName}>
+                        <button
+                            onMouseEnter={e => {
+                                e.currentTarget.style.backgroundColor = 'black'
+                                e.currentTarget.style.color = 'white'
+                            }}
+                            onMouseLeave={e => {
+                                e.currentTarget.style.backgroundColor = ''
+                                e.currentTarget.style.color = 'black'
+                            }}
+                            style={{ transition: 'all 0.4s ease' }}
+                            className={`border border-black font-semibold tracking-[2px] w-44 text-black text-[12px] px-[28px] py-[13.5px] rounded-[2.5px]`}>SEE PRODUCT</button>
+                    </Link>
+                </div>
+            </div>
+        </>
     );
 }
 
--- src/components/Home/HomeProducts.jsx
+++ src/components/Home/HomeProducts.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../Home/HomeCard';
 import HomeZX9Speaker from '../../assets/home-zx9-speaker.png';
 import HomeZX7Speaker from '../../assets/home-zx7-speaker.png';
+import HomeZX7SpeakerTablet from '../../assets/home-zx7-speaker-tab.jpg';
 import HomeProductEarphones from '../../assets/home-earphones.jpg';
 import HomeCardBig2 from './HomeCardBig2';
 import HomeCardBig1 from './HomeCardBig1';
@@ -13,7 +14,7 @@ function HomeProducts() {
             <div className='w-full 2xl:px-[135px] xl-custom:px-[130px] xl:px-32 lg-custom:px-12 lg:px-7 px-5 flex justify-center items-center'>
                 <div className='w-full'>
                     <HomeCardBig1 isResponsive={true} pathName="./speakers/zx9" productImage={HomeZX9Speaker} bgColor="#d87d4a" titleColor="white" titleLetterSpacing='7px' titleName="ZX9 SPEAKER" />
-                    <HomeCardBig2 pathName="./speakers/zx7" productImage={HomeZX7Speaker} mainContImg={false} titleName="ZX7 SPEAKER" titleColor="black" titleSize="30px" imageOnLeft={false} />
+                    <HomeCardBig2 pathName="./speakers/zx7" bgImage={HomeZX7Speaker} bgImageTablet={HomeZX7SpeakerTablet} mainContImg={false} titleName="ZX7 SPEAKER" titleColor="black" titleSize="30px" imageOnLeft={false} />
                     <div className="grid md:grid-cols-2 md:grid-rows-1 grid-rows-2 xl:gap-8 lg-custom:gap-7 lg:gap-6 gap-5">
                         <Card isBgImage={true} bgImage={HomeProductEarphones} />
                         <Card pathName="./earphones/yx1" isHeading={true} titleName="YX1 EARPHONES" isButton={true} padding="96px" />
